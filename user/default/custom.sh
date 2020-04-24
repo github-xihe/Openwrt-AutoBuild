15d2444f045744c550f22b0f6612e71fb4059bfe
hyird <31695447+hyird@users.noreply.github.com>
2020-04-24 15:32:15 +0800

    Update custom.sh

--- user/default/custom.sh
+++ user/default/custom.sh
@@ -6,8 +6,8 @@ echo "Test custom.sh"
 ./scripts/feeds install -a
 
 #修改openwrt_release版本
-sed -i 's#R[0-9].*\('\''\)#R20.4.20\1#g'   package/lean/default-settings/files/zzz-default-settings
-sed -i 's/https:/R20.4.20/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's#R[0-9].*\('\''\)#R20.4.24\1#g'   package/lean/default-settings/files/zzz-default-settings
+sed -i 's/https:/R20.4.24/g' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/http:/snapshots/g' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/openwrt.proxy.ustclug.org/op.hyird.xyz/g' package/lean/default-settings/files/zzz-default-settings
 #去除密码
